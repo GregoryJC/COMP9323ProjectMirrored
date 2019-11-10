30c178eb2638b3f63ef4122c54fbab8b6fbde536
Alan Koo <alankoo1995@gmail.com>
2019-11-10 17:16:51 +1100

    fix cors in global config

---
+++ backend/src/main/java/comp9323/group12/backend/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package comp9323.group12.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig {
+  @Bean
+  public WebMvcConfigurer allowCorsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+      }
+    };
+  }
+}
