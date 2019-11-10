9e33d334cd9b0b051c0d28deddfbca04559de4ff
Alan Koo <alankoo1995@gmail.com>
2019-11-10 20:24:38 +1100

    hot fix

--- backend/src/main/java/comp9323/group12/backend/config/SecurityConfig.java
+++ backend/src/main/java/comp9323/group12/backend/config/SecurityConfig.java
@@ -3,8 +3,11 @@ package comp9323.group12.backend.config;
 
 import comp9323.group12.backend.component.auth.RestAuthenticationFailureHandler;
 import comp9323.group12.backend.component.auth.RestAuthenticationSuccessHandler;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -58,9 +61,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(Arrays.asList("*"));
     configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS", "PUT", "DELETE"));
+    configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept",
+            "Authorization", "Access-Control-Allow-Credentials", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods",
+            "Access-Control-Allow-Origin", "Access-Control-Expose-Headers", "Access-Control-Max-Age",
+            "Access-Control-Request-Headers", "Access-Control-Request-Method", "Age", "Allow", "Alternates",
+            "Content-Range", "Content-Disposition", "Content-Description"));
+    configuration.setAllowCredentials(true);
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
   }
+
+//  @Bean
+//  public FilterRegisterationBean conrsfileter() {
+//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    CorsConfiguration config = new CorsConfiguration();
+//    config.setAllowCredentials(true);
+//    config.addAllowedOrigin("*");
+//    config.addAllowedHeader("*");
+//    config.addAllowedMethod("*");
+//    source.registerCorsConfiguration("/**", config);
+//    FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+//    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//  }
+//@Bean
+//public FilterRegistrationBean corsFilter() {
+//  UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//  CorsConfiguration config = new CorsConfiguration();
+//  config.setAllowCredentials(true);
+//  config.addAllowedOrigin("*");
+//  config.addAllowedHeader("*");
+//  config.addAllowedMethod("*");
+//  source.registerCorsConfiguration("/**", config);
+//  FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+//  bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//  return bean;
+//}
 }
 
