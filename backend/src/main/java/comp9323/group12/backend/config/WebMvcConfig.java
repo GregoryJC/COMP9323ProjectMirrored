b66cf40ea5bd2db7191a133df7cb2873a2166831
Alan Koo <alankoo1995@gmail.com>
2019-11-10 19:39:43 +1100

    hotfix cors

--- backend/src/main/java/comp9323/group12/backend/config/WebMvcConfig.java
+++ backend/src/main/java/comp9323/group12/backend/config/WebMvcConfig.java
@@ -5,20 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebMvcConfig {
-  @Bean
-  public WebMvcConfigurer allowCorsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowedMethods("PUT", "DELETE", "POST", "OPTIONS", "GET")
-                .allowCredentials(false)
-                .maxAge(3600);
-      }
-    };
-  }
-}
+//@Configuration
+//public class WebMvcConfig {
+//  @Bean
+//  public WebMvcConfigurer allowCorsConfigurer() {
+//    return new WebMvcConfigurer() {
+//      @Override
+//      public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedHeaders("Access-Control-Allow-Origin")
+//                .allowedMethods("PUT", "DELETE", "POST", "OPTIONS", "GET")
+//                .allowCredentials(false)
+//                .maxAge(3600);
+//      }
+//    };
+//  }
+//}
