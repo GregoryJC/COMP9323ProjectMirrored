47adcb2c2883c1d5b4db60e4960094797d7d4be3
Alan Koo <alankoo1995@gmail.com>
2019-11-11 12:57:58 +1100

    add profile response & fix question raw json response issues

--- backend/src/main/java/comp9323/group12/backend/mapper/RelUserSkillVerifiedMapper.java
+++ backend/src/main/java/comp9323/group12/backend/mapper/RelUserSkillVerifiedMapper.java
@@ -2,7 +2,10 @@ package comp9323.group12.backend.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.dao.DataAccessException;
+
+import java.util.List;
 
 @Mapper
 public interface RelUserSkillVerifiedMapper {
@@ -10,5 +13,8 @@ public interface RelUserSkillVerifiedMapper {
   @Insert("INSERT INTO rel_user_skill_verified(username, skill) SELECT #{username}, " +
           "related_skill FROM quiz WHERE quiz.id = #{quizId}")
   public int verifySkill(String username, Integer quizId) throws DataAccessException;
+
+  @Select("SELECT skill FROM rel_user_skill_verified WHERE username = #{username}")
+  public List<String> getSkillsByUsername(String username);
 
 }
