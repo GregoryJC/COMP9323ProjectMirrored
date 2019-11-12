ff93e73b4b5830605416fc19204e2b470413f0e1
Modrisco <menglingxu95@gmail.com>
2019-11-12 21:46:52 +1100

    build version

---
+++ frontend/build/precache-manifest.bd7fdda85381781ed13b700103443bb9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057233a0468ee28cddaa694202a6a68b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c15d05f167e14beea46d",
+    "url": "/static/css/2.cc2fc9c7.chunk.css"
+  },
+  {
+    "revision": "536fb4cdf794e8ee6289",
+    "url": "/static/css/main.51fb48b1.chunk.css"
+  },
+  {
+    "revision": "c15d05f167e14beea46d",
+    "url": "/static/js/2.fe7e15fc.chunk.js"
+  },
+  {
+    "revision": "536fb4cdf794e8ee6289",
+    "url": "/static/js/main.ab811eae.chunk.js"
+  },
+  {
+    "revision": "daf37efea5ce01eae8db",
+    "url": "/static/js/runtime-main.28f6ede9.js"
+  },
+  {
+    "revision": "ebbdd2b875af4b326e6c845662a2ec64",
+    "url": "/static/media/login-background.ebbdd2b8.jpg"
+  },
+  {
+    "revision": "328f2306aa80c84bcb3f91cda5f7f12e",
+    "url": "/static/media/logo.328f2306.png"
+  },
+  {
+    "revision": "862a9117c8121cc4269fd2930fdca0aa",
+    "url": "/static/media/signin-desktop-illustration.862a9117.jpg"
+  }
+]);
